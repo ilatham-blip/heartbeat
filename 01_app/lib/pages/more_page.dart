923ea79bd9097fc84695ac59970683864d8c566b
peternemeth05 <pnemethasd@gmail.com>
2026-01-25 21:43:57 +0000

    modified login page, added admin login

--- 01_app/lib/pages/more_page.dart
+++ 01_app/lib/pages/more_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:heartbeat/app_state.dart';
 
-class PlaceholderPage extends StatelessWidget{
+class MorePage extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
